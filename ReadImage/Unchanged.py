227195a6f75e8f57124271d1074693f07405c0da
Samia-00 <samia.201315@gmail.com>
2021-10-18 11:51:10 +0600

    unchanged image

---
+++ ReadImage/Unchanged.py
@@ -0,0 +1,11 @@
+import cv2
+image = cv2.imread('image.jpeg')
+window_name ="Display Image"
+
+
+#img=cv2.imread("image.jpeg",cv2.IMREAD_UNCHANGED)
+img=cv2.imread("image.jpeg",0)
+cv2.imshow(window_name,img)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
