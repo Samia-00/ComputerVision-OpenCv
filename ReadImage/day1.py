2a2ade66509e792544b9cde423434643fb191d46
Samia-00 <samia.201315@gmail.com>
2021-10-18 11:47:40 +0600

    read image

---
+++ ReadImage/day1.py
@@ -0,0 +1,8 @@
+import cv2
+image = cv2.imread('image.jpeg')
+window_name ="Display Image"
+image=cv2.imread("image.jpeg",cv2.IMREAD_GRAYSCALE)
+
+cv2.imshow(window_name,image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
